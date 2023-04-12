eda0a1c61e1a80cbb52b2215938697d847c7371a
George Ogden <george@goprogram.ai>
2023-04-12 12:43:51 +0100

    refactor QPE

--- src/circuits/qpe.py
+++ src/circuits/qpe.py
@@ -7,27 +7,21 @@ from .qft import InverseQuantumFourierTransform as IQFT
 class QuantumPhaseEstimation(Circuit):
     def __init__(self, t: int, controlled_gate: ControlledGate):
         assert controlled_gate.t == 1, "Controlled unity must have a single controlled qubit gate"
-        gates = []
-        gates.append(Gate.parallel(*[Hadamard] * t) + Gate.Identity(controlled_gate.n))
-        big_swap = []
-        for i in range(controlled_gate.n):
-            big_swap.append(
-                (Gate.Identity(i) + SWAP + Gate.Identity(controlled_gate.n - i - 1))
-            )
-        big_swap = Gate.serial(*big_swap)
-        oracle = big_swap * controlled_gate * big_swap.inverse
+        gates = [Gate.parallel(*[Hadamard] * t) + Gate.Identity(controlled_gate.n)]
+        oracle_swap = Gate.serial(
+            *[
+                Gate.Identity(i) + SWAP + Gate.Identity(controlled_gate.n - i - 1)
+                for i in range(controlled_gate.n)
+            ]
+        )
+        oracle = oracle_swap * controlled_gate * oracle_swap.inverse
         for i in range(t):
             swap = Gate.Identity(t - i - 1) + Gate.Swap(i) + Gate.Identity(controlled_gate.n)
-            print(swap)
-            gates.append(
+            gates.extend([
+                swap,
+                Gate.Identity(t - 1) + Gate.serial(*[oracle] * (2 ** i)),
                 swap
-            )
-            gates.append(
-                Gate.Identity(t - 1) + Gate.serial(*[oracle] * (2 ** i))
-            )
-            gates.append(
-                swap
-            )
+            ])
 
         gates.append(IQFT(t).to_gate() + Gate.Identity(controlled_gate.n))
         super().__init__(gates, "Quantum Phase Estimation")
