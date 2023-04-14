12234670d8970706ed0374ce5e56534621e2f47a
George Ogden <george@goprogram.ai>
2023-04-14 12:34:13 +0100

    update tests for different algorithmic structure

--- tests/test_algorithms/test_deutsch_jozsa.py
+++ tests/test_algorithms/test_deutsch_jozsa.py
@@ -1,4 +1,4 @@
-import jax.numpy as jnp
+import numpy as np
 
 from quantum.algorithms import DeutschJoszaAlgorithm
 from quantum.gates import Oracle
@@ -7,28 +7,40 @@ def test_deutsch_jozsa_algorithm_balanced_1():
     table = [0, 0, 1, 1]
     oracle = Oracle.from_table(table)
     algorithm = DeutschJoszaAlgorithm(oracle)
-    assert jnp.allclose(algorithm(), 1.)
+    assert np.allclose(algorithm(), [1., 0.])
 
 def test_deutsch_jozsa_algorithm_balanced_2():
     table = [0, 1, 0, 1]
     oracle = Oracle.from_table(table)
     algorithm = DeutschJoszaAlgorithm(oracle)
-    assert jnp.allclose(algorithm(), 1.)
+    assert np.allclose(algorithm(), [0., 1.])
 
 def test_deutsch_jozsa_algorithm_balanced_3():
     table = [1, 0, 0, 1]
     oracle = Oracle.from_table(table)
     algorithm = DeutschJoszaAlgorithm(oracle)
-    assert jnp.allclose(algorithm(), 1.)
+    assert np.allclose(algorithm(), [1., 1.])
+
+def test_deutsch_jozsa_algorithm_balanced_4():
+    table = [1, 0] * 8
+    oracle = Oracle.from_table(table)
+    algorithm = DeutschJoszaAlgorithm(oracle)
+    assert np.allclose(algorithm(), [0., 0., 0., 1.])
 
 def test_deutsch_jozsa_algorithm_constant_1():
     table = [0, 0, 0, 0]
     oracle = Oracle.from_table(table)
     algorithm = DeutschJoszaAlgorithm(oracle)
-    assert jnp.allclose(algorithm(), 0.)
+    assert np.allclose(algorithm(), [0., 0.])
 
 def test_deutsch_jozsa_algorithm_constant_2():
     table = [1, 1, 1, 1]
     oracle = Oracle.from_table(table)
     algorithm = DeutschJoszaAlgorithm(oracle)
-    assert jnp.allclose(algorithm(), 0.)
+    assert np.allclose(algorithm(),[ 0., 0.])
+
+def test_deutsch_jozsa_algorithm_constant_3():
+    table = [0] * 16
+    oracle = Oracle.from_table(table)
+    algorithm = DeutschJoszaAlgorithm(oracle)
+    assert np.allclose(algorithm(), [0., 0., 0., 0.])
